1b5b9375381ef3c437f7ea072fddd5bb89df7385
Camila <camilakeikosm@gmail.com>
2021-09-14 01:41:21 -0300

    Adjust css

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// slider produtos
+
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#splide', {
+        type: 'loop',
+        fixedHeight: 400,
+        gap: 20,
+        autoWidth: true,
+        lazyLoad: 'nearby',
+        focus: 'center',
+	    perPage: 4,
+        breakpoints: {
+            640: {
+                perPage: 1,
+                gap: 5,
+            },
+        },
+    } ).mount();
+} );
